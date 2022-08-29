c839787e92ad2b03e83140f9b56f07d89893aed3
Mohamad Adithya <mohamadadithya@gmail.com>
2022-08-29 11:47:44 +0700

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { page } from '$app/stores';
+    import Seo from '../components/SEO.svelte';
+    const error = $page
+</script>
+
+<Seo title="Error {error.status} | Quranible" />
+
+<section class="text-center">
+        <div>
+            <h1 class="text-3xl font-bold mb-3">Error {error.status}</h1>
+            {#if error.status == 404 }
+                <p>Halaman Tidak ditemukan</p>
+            {:else if error.status == 500}
+                <p>Sepertinya terdapat kesalahan di sisi server</p>
+            {/if}
+        </div>
+</section>
