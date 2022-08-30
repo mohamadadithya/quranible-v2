c3303a2cd9562ae3c34c524c97f7789a987a78a7
Mohamad Adithya <mohamadadithya@gmail.com>
2022-08-30 14:25:31 +0700

    add hotjar script

--- src/components/SEO.svelte
+++ src/components/SEO.svelte
@@ -34,5 +34,14 @@
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
     gtag('config', 'G-W6TYYSP7NT');
+    // Hotjar Tracking Code for https://quranible-v2.vercel.app
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3130647,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
     </script>
 </svelte:head>
